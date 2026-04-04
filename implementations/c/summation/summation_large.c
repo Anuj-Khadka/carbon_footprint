6b1086f29f6932c690de4da7895dd3373686812a
Anuj-Khadka <anujkhadka008@gmail.com>
2026-04-04 15:03:38 -0400

    update c summation implementations, remove benchmark results

---
+++ implementations/c/summation/summation_large.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#define N 1000000
+
+static int64_t arr[N];
+
+static void setup(void) {
+    for (int i = 0; i < N; i++) {
+        arr[i] = (int64_t)i + 1;
+    }
+}
+
+static int64_t summation(void) {
+    int64_t sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main(void) {
+    setup();
+
+    printf("ready\n");
+    fflush(stdout);
+
+    char buf[64];
+    while (fgets(buf, sizeof(buf), stdin)) {
+        printf("%" PRId64 "\n", summation());
+        fflush(stdout);
+    }
+
+    return 0;
+}
