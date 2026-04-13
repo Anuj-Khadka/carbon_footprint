c0a0b9698b456e35bbe317cc84d2d274e6593aa5
Anuj-Khadka <anujkhadka008@gmail.com>
2026-04-13 01:53:52 +0000

    Add matrix multiplication implementations for small, mid, and large matrix sizes

---
+++ implementations/java/matrix_multiplication/MatrixMultiplication_large.java
@@ -0,0 +1,46 @@
+package matrix_multiplication;
+
+import java.io.*;
+
+public class MatrixMultiplication_Large {
+    static final int N = 500;
+    static long[][] a = new long[N][N];
+    static long[][] b = new long[N][N];
+    static long[][] c = new long[N][N];
+
+    static void setup() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                a[i][j] = (long)i + j;
+                b[i][j] = (long)i - j;
+            }
+        }
+    }
+
+    static long matrixMul() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                long acc = 0;
+                for (int k = 0; k < N; k++) {
+                    acc += a[i][k] * b[k][j];
+                }
+                c[i][j] = acc;
+            }
+        }
+        return c[N / 2][N / 2];
+    }
+
+    public static void main(String[] args) throws IOException {
+        setup();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("ready");
+        System.out.flush();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(matrixMul());
+            System.out.flush();
+        }
+    }
+}
