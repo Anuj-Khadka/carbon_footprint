15b36aaf22bba6d788baab602547a29cdc06d4fb
Anuj-Khadka <anujkhadka008@gmail.com>
2026-04-13 02:00:39 +0000

    Add three Go implementations for summation with varying array sizes and remove a temporary sync file

---
+++ implementations/go/summation/summation_mid.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const N = 10000
+
+var arr [N]int64
+
+func setup() {
+	for i := 0; i < N; i++ {
+		arr[i] = int64(i + 1)
+	}
+}
+
+func summation() int64 {
+	var sum int64 = 0
+	for i := 0; i < N; i++ {
+		sum += arr[i]
+	}
+	return sum
+}
+
+func main() {
+	setup()
+
+	fmt.Println("ready")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(summation())
+	}
+}
