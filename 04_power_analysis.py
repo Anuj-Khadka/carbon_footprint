f77914d21b42282c37fb2d6e25b0b77082202dbf
Anuj-Khadka <anujkhadka008@gmail.com>
2026-04-23 03:31:00 +0000

    Add power analysis calculations and improve function documentation

---
+++ 04_power_analysis.py
@@ -0,0 +1,40 @@
+"""
+LHM web server connect
+Electricity Map API
+j -> kWh -> gCO2e
+
+"""
+
+import os
+import requests
+import sys
+import time
+import json
+import argparse
+import urllib.request
+import urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+
+LHM_URL = "http://172.22.1.29:8085/data.json"
+EM_ZONE = "US-NY-NYIS"
+EMAP_API_KEY = "zyjqZja8pJXqecWbs6d2"
+EM_URL = f"https://api.electricitymaps.com/v3/carbon-intensity/latest?zone={EM_ZONE}"
+SAMPLE_SECS   = 5      # how long to sample LHM wattage
+SAMPLE_HZ     = 4      # polls per second
+
+
+# response = requests.get(
+#     # "https://api.electricitymaps.com/v3/carbon-intensity/latest",
+#     EM_URL,
+#     headers={
+#         "auth-token": EMAP_API_KEY
+#     }
+# )
+# print(response.json())
+
+def bold(s):  return f"\033[1m{s}\033[0m"
+def green(s): return f"\033[92m{s}\033[0m"
+def red(s):   return f"\033[91m{s}\033[0m"
+def yellow(s):return f"\033[93m{s}\033[0m"
